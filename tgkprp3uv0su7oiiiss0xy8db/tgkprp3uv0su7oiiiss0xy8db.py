b81e5fdf2df9e032523cfd9e473a368fd97676c0
Jevan Goh <javerngoh@gmail.com>
2026-09-23 21:54:50 +0800

    v0.0.2

--- tgkprp3uv0su7oiiiss0xy8db/tgkprp3uv0su7oiiiss0xy8db.py
+++ tgkprp3uv0su7oiiiss0xy8db/tgkprp3uv0su7oiiiss0xy8db.py
@@ -6,19 +6,19 @@ from structlog import BoundLogger
 
 
 class tgkprp3uv0su7oiiiss0xy8db(BoundLogger):
-    def record(self, event: str, **kwargs: Any):
+    def record(self, event: str, **kwargs: Any) -> Any:
         return self._proxy_to_logger("record", event, **kwargs)
 
-    def inform(self, event: str, **kwargs: Any):
+    def inform(self, event: str, **kwargs: Any) -> Any:
         return self._proxy_to_logger("inform", event, **kwargs)
 
-    def notify(self, event: str, **kwargs: Any):
+    def notify(self, event: str, **kwargs: Any) -> Any:
         return self._proxy_to_logger("notify", event, **kwargs)
 
-    def alert(self, event: str, **kwargs: Any):
+    def alert(self, event: str, **kwargs: Any) -> Any:
         return self._proxy_to_logger("alert", event, **kwargs)
 
-    def warn(self, event: str, **kwargs: Any):
+    def warn(self, event: str, **kwargs: Any) -> Any:
         return self._proxy_to_logger("warn", event, **kwargs)
 
     def add_handler(self, handler: logging.Handler) -> tgkprp3uv0su7oiiiss0xy8db:
